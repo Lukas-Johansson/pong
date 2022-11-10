8ba9e2f5c992c5810567bc9394b7dc0fd286bd27
Lukas-Johansson <lukas.johansson4@elev.ga.ntig.se>
2022-11-10 13:34:47 +0100

    Boll och paddle :)

---
+++ src/ball.java
@@ -0,0 +1,25 @@
+public class ball {
+    private int x;
+    private int y;
+    private int vx;
+    private int vy;
+
+    public ball(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move(){
+        x = vx;
+        y = vy;
+    }
+
+    public void bounce(paddle p){
+
+    }
+
+    public void bounce() {
+        vx += vx;
+        vy += vx;
+    }
+}
--- src/paddle.java
+++ src/paddle.java
@@ -1,6 +1,31 @@
 public class paddle {
     private int x;
     private int y;
-    private int width;  
+    private int width = 10;
 
+    public paddle(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void moveUp() {
+        y -= 1;
+    }
+
+    public void moveDown() {
+        y += 1;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
 }
+
