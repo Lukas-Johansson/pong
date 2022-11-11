4c69adc08ab03bbaf08c919acb1a5478f2a73c70
Lukas-Johansson <lukas.johansson4@elev.ga.ntig.se>
2022-11-11 13:27:55 +0100

    Mer på bollen fått bounce(paddle p) kanske att fungera

--- src/ball.java
+++ src/ball.java
@@ -4,22 +4,36 @@ public class ball {
     private int vx;
     private int vy;
 
-    public ball(int x, int y) {
+    public ball(int x, int y, int vx, int vy) {
         this.x = x;
         this.y = y;
+        this.vx = 1;
+        this.vy = 1;
     }
 
     public void move(){
-        x = vx;
-        y = vy;
-    }
-
-    public void bounce(paddle p){
-
+        x += vx;
+        y += vy;
     }
 
     public void bounce() {
-        vx += vx;
-        vy += vx;
+        vy -= vy;
     }
+
+    public void bounce(paddle p){
+        p.getX();
+        p.getY();
+        p.getWidth();
+
+        if ((p.getY() >= y) && (y <= p.getWidth())){
+            if (p.getX() + p.getY() == this.x + this.y) {
+                vx -= vx;
+            }
+        }
+
+
+
+    }
+
+
 }
